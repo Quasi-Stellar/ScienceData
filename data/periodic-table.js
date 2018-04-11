d74e3514ce63a807782eb1465fa9f56c892e62f3
Aurolux <36804393+Aurolux@users.noreply.github.com>
2018-04-10 22:30:55 -0500

    Create periodic-table.js

---
+++ data/periodic-table.js
@@ -0,0 +1,95 @@
+'use strict';
+
+exports.ChemicalElements = {
+        "hydrogen": {
+                    desc: "Symbol: H | Atomic Number: 1."
+              },
+        "helium": {
+                    desc: "Symbol: He | Atomic Number: 2"
+              },
+         "lithium": {
+                    desc: "Symbol: Li | Atomic Number: 3"
+              },
+         "berylium": {
+                    desc: "Symbol: Be | Atomic Number: 4"
+              },
+         "boron": {
+                    desc: "Symbol: B | Atomic Number: 5"
+              },
+         "carbon": {
+                    desc: "Symbol: C | Atomic Number: 6"
+              },
+         "nitrogen": {
+                    desc: "Symbol: N | Atomic Number: 7"
+              },
+         "oxygen": {
+                    desc: "Symbol: O | Atomic Number: 8"
+              },
+         "fluorine": {
+                    desc: "Symbol: F | Atomic Number: 9"
+              },
+         "neon": {
+                    desc: "Symbol: Ne | Atomic Number: 10"
+              },
+         "sodium": {
+                    desc: "Symbol: Na | Atomic Number: 11"
+              },
+         "magnesium": {
+                    desc: "Symbol: Mg | Atomic Number: 12"
+              },
+         "aluminum": {
+                    desc: "Symbol: Al | Atomic Number: 13"
+              },
+         "silicon": {
+                    desc: "Symbol: Si | Atomic Number: 14"
+              }, 
+         "phosphorus": {
+                    desc: "Symbol: P | Atomic Number: 15"
+              },
+         "sulfur": {
+                    desc: "Symbol: S | Atomic Number: 16"
+              },
+         "chlorine": {
+                    desc: "Symbol: Cl | Atomic Number: 17"
+              },
+         "argon": {
+                    desc: "Symbol: Ar | Atomic Number: 18"
+              },
+         "potassium": {
+                    desc: "Symbol: K | Atomic Number: 19"
+              },
+         "calcium": {
+                    desc: "Symbol: Ca | Atomic Number: 20"
+              },
+         "scandium": {
+                    desc: "Symbol: Sc | Atomic Number: 21"
+              },
+         "titanium": {
+                    desc: "Symbol: Ti | Atomic Number: 22"
+              },
+         "vanadium": {
+                    desc: "Symbol: V | Atomic Number: 23"
+              },
+         "chromium": {
+                    desc: "Symbol: Cr | Atomic Number: 24"
+              },
+         "manganese": {
+                    desc: "Symbol: Mn | Atomic Number: 25"
+              },
+         "iron": {
+                    desc: "Symbol: Fe | Atomic Number: 26"
+              },
+         "cobalt": {
+                    desc: "Symbol: Co | Atomic Number: 27"
+              },
+         "nickel": {
+                    desc: "Symbol: Ni | Atomic Number: 28"
+              },
+         "copper": {
+                    desc: "Symbol: Cu | Atomic Number: 29"
+              },
+         "zinc": {
+              desc: "Symbol: Zn | Atomic Number: 30"
+              },
+};
+                   
