0823ef85a7a919bbeb2f699d2207756ed8df0b1e
Aurolux <36804393+Aurolux@users.noreply.github.com>
2018-04-12 23:19:47 -0500

    Add Science Types for sciences.js as a game

---
+++ data/sciences.js
@@ -0,0 +1,115 @@
+use strict';
+
+exports.ScienceTypes = {
+        "Aerodynamics": {
+                desc: "The study of the dynamics of gases; Science of movement in a flow of air or gas.",
+                name: "Aerodynamics"
+              },
+        "Agrology": {
+                desc: "The study of the application of science to agriculture.",
+                name: "Agrology"
+              },
+        "Anatomy": {
+                desc: "The study of the structure of a body.",
+                name: "Anatomy"
+              },
+        "Anthropology": {
+                desc: "The study of human cultures.",
+                name: "Anthropology"
+              },
+        "Astronomy": {
+                desc: "The study of celestial bodies.",
+                name: "Astronomy"
+              },
+        "Astrophysics": {
+                desc: "The study of the behavior of astronomical figures.",
+                name: "Astrophysics"
+              },
+       "Biology": {
+                desc: "The study of life.",
+                name: "Biology"
+              },
+       "Botany": {
+                desc: "The study of life specifically in kingdom plantae.",
+                name: "Botany"
+              },
+        "Cardiology": {
+                desc: "The study of the heart.",
+                name: "Cardiology"
+              },
+        "Cartography": {
+                desc: "The study of making geographical maps and globes.",
+                name: "Botany"
+              },
+        "Chemistry": {
+                desc: "The study of substances and their properties.",
+                name: "Chemistry"
+              },
+        "Climatology": {
+                desc: "The study of climate.",
+                name: "Climatology"
+              },
+        "Cosmetology": {
+                desc: "The study of cosmetics.",
+                name: "Cosmetology"
+              },
+        "Cosmology": {
+                desc: "The study of the universe and its origin",
+                name: "Cosmology"
+              },
+        "Cryptology": {
+                desc: "The study of code.",
+                name: "Cryptology"
+              },
+        "Dendrochronology": {
+                desc: "The study of tree rings.",
+                name: "Dendrochronology"
+              },
+        "Geology": {
+                desc: "The study of the earth and its features.",
+                name: "Geology"
+             },
+        "Ecology": {
+                desc: "The study of organisms in relation to one another.",
+                name: "Ecology"
+              },
+        "Economics": {
+                desc: "The branch of knowledge concerned with the production, consumption, and transfer of wealth.",
+                name: "Economics"
+        "Garbology": {
+                desc: "The study of trash and its effects.",
+                name: "Garbology"
+              },
+       "Gastroenterology": {
+                desc: "The study of the stomach and intestines.",
+                name: "Gastroenertology"
+             },
+       "Geology": {
+                desc: "The study of the earth and its features.",
+                name: "Geology"
+             },
+      "Geography": {
+                desc: "The study of the earth and its features.",
+                name: "Geography"
+             },
+      "Geogony": {
+                desc: "The study of the earth's formation.",
+                name: "Geogony"
+             },
+      "Heliology": {
+                desc: "The study of the sun.",
+                name: "Heliology"
+             },
+      "Helioseismology": {
+                desc: "The study of the sun's interior inferred by observing its surface oscillations.",
+                name: "Helioseismology"
+             },
+      "Hematology": {
+                desc: "The study of blood.",
+                name: "Hematology"
+             },
+      "Geogony": {
+                desc: "The study of the earth's formation.",
+                name: "Geogony"
+             },
+};
